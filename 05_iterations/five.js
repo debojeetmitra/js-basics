eec7fcbdef472fb14859880b7c0c4a28949113ae
Debojeet Mitra <142051979+debojeetmitra@users.noreply.github.com>
2026-05-02 10:34:10 +0000

    forof and foreach loop done

---
+++ 05_iterations/five.js
@@ -0,0 +1,43 @@
+const coding = ["java", "python", "cpp", "c"]
+
+// coding.forEach( function (item) {
+//    console.log(item);
+   
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);
+    
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=>{
+//     console.log(item, index, arr);
+    
+// })
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+     {
+        languageName: "python",
+        languageFileName: "py"
+    },
+     {
+        languageName: "C++",
+        languageFileName: "cpp"
+    }
+]
+
+myCoding.forEach( (item)=>{
+    console.log(item.languageFileName);
+    
+})
