f4d9d0c3592dd0d2bb05a10cad6e108d21786116
Debojeet Mitra <142051979+debojeetmitra@users.noreply.github.com>
2026-05-05 16:05:14 +0000

    Objects basics done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -13,14 +13,34 @@ const mySym = Symbol("key1")
 const JsUser = {
     name: "Debo",
     "full name":"Debojeet Mitra",
-    [mySym]: "myKey1",
+    "mySym": "myKey1",
     age: 22,
     location: "Jaipur",
     email: "debo@gmail.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 } 
-console.log((JsUser.email));
-console.log(JsUser["email"])
-console.log(JsUser["full name"]);
-console.log(JsUser[mySym]);
+// console.log((JsUser.email));
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "debojeet@chatgpt.com"
+//Object.freeze(JsUser)
+JsUser.email = "hitesh@chatgpt.com"
+// console.log(JsUser);
+
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");
+    
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User, ${this.name}`);
+    
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
