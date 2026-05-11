c5686037d0ff4be2fd55b075eddfea1c4ab5fa02
Debojeet Mitra <142051979+debojeetmitra@users.noreply.github.com>
2026-05-11 14:10:04 +0000

    JSON Basics

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -46,9 +46,39 @@ const users = [
 ]
 
 users[1].id
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Debo"
+}
+
+//course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+
+// {
+//     "name": "Debo",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+
+[
+    {},
+    {},
+    {}
+]
+
+
